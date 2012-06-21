18165300dd291c97460e846bb4d25d2c45ba5bfc
Arturo Fernandez <arturo@bsnux.com>
2012-06-21 12:16:31 +0200

    Script for installing MATE desktop on Fedora

---
+++ install_mate_fedora.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+
+yum install http://dl.dropbox.com/u/49862637/Mate-desktop/mate-desktop-fedora/releases/16/noarch/mate-desktop-release-16-5.fc16.noarch.rpm
+
+yum groupinstall MATE-Desktop
